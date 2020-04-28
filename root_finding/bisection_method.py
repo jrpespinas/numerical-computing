ea3620600fa744671cc0cb4eb14a88573c4b84f6
Jan Rodolf Espinas <jrpespinas@gmail.com>
2020-04-28 23:40:43 +0800

    chore: create template for bisection_method.py

---
+++ root_finding/bisection_method.py
@@ -0,0 +1,14 @@
+"""Bisection Method using Python"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+__version__: '1.0.0'
+__author__: 'Jan Rodolf Espinas'
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
