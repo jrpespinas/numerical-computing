60cbade7d877ba76be1e26edd3999d3347f5b9f4
Jan Rodolf Espinas <jrpespinas@gmail.com>
2020-05-17 21:40:55 +0800

    docs: add docstring to all functions

--- root_finding/bisection_method.py
+++ root_finding/bisection_method.py
@@ -19,45 +19,89 @@ from __future__ import print_function
 
 import sys
 
-__version__: '1.0.0'
-__author__: 'Jan Rodolf Espinas'
+__version__ = '1.0.0'
+__author__ = 'Jan Rodolf Espinas'
 
 
 TOL = 1e-6
 
+
 def f(x):
+    """
+    A function that evaluates `x`.
+
+    Parameters
+    ----------
+    x : int or float
+        Any value to be evaluated
+
+    Returns
+    -------
+    float
+        the output of the function
+
+    """
     return (x ** 3) + (4 * (x ** 2)) - 10
 
-def check_root(a,b):
-	if f(a) * f(b) < 0:
-		return True
 
-def bisection_method(a,b,iterations):
-	if check_root(a, b):
-		i = 0
-		while i < iterations:
-			c = (a + b) / 2
-			root = f(c)
-			if root == 0 or (b - a)/2 <= TOL:
-				break
+def check_root(a, b):
+    """
+    Verify if the root exist by checking the signs of `a` and `b`.
 
-			if root * f(a) < 0:
-				b = c
-			else:
-				a = c
-			
-			if len(sys.argv) > 1:
-				print(\
-					f'{i+1}: a={a:.6f}, b={b:.6f}, c={c:.6f}, root={root:.6f},')
-			else:
-				print(f'{i+1}: root = {root:.6f}')
+    Parameters
+    ----------
+    a : int
+        Left endpoint
+    b : int
+        Right endpoint
 
-			i += 1			
-	else:
-		print("Root does not exist")
-	
-def main():
-	bisection_method(1,2,20)
+    Return
+    ------
+    bool:
+        Returns True if a root exist
+
+    """
+    if f(a) * f(b) < 0:
+        return True
+
+
+def bisection_method(a, b, iterations):
+    """
+    A root-finding algorithm which uses an iterative method.
+
+    Parameters
+    ----------
+    a : int or float
+        left endpoint
+    b : int or float
+        right endpoint
+    iterations : int
+        number of iterations
+
+    """
+    if check_root(a, b):
+        i = 0
+        while i < iterations:
+            c = (a + b) / 2
+            root = f(c)
+            tol = (b - a) / 2
+            if root == 0 or tol <= TOL:
+                break
+            if root * f(a) < 0:
+                b = c
+            else:
+                a = c
+
+            verbosity = f'{i+1}:\ta={a:.6f},\tb={b:.6f},\tc={c:.6f},\troot={root:.6f}'
+            if len(sys.argv) > 1:
+                print()
+            else:
+                print(f'{i+1}:\t root = {root:.6f},\ttolerance = {tol:.6f}')
+
+            i += 1
+        else:
+            print("Root does not exist")
+
 
 if __name__ == '__main__':
-    main()
+    bisection_method(1, 2, 20)
