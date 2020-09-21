198f6d1151c27807bbfb20ba87ce800568d6a797
Jan Rodolf Espinas <jrpespinas@gmail.com>
2020-09-21 22:11:52 +0800

    feat: initialize TaylorSeries class

--- fundamentals/taylor_series.py
+++ fundamentals/taylor_series.py
@@ -18,8 +18,15 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import math
+
 __version__ = '1.0.0'
 __author__ = 'Jan Rodolf Espinas'
+
+class TaylorSeries(object):
+    def __init__(self, num_terms: int):
+        self.num_terms = num_terms
+    
 
 def main():
     pass
