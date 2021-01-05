ac498d025dcb1b70a76e3f79199ebb8d7142b78c
jrpespinas <jrpespinas@gmail.com>
2021-01-05 15:17:06 +0800

    chore: add gaussian_elimination method

--- systems_of_linear_equations/gaussian_elimination.py
+++ systems_of_linear_equations/gaussian_elimination.py
@@ -1,8 +1,19 @@
 """Gaussian Elimination"""
 
+import numpy as np
+
+
+def gaussian_elimination(matrix: np.ndarray):
+    return matrix
+
 
 def main():
-    pass
+    matrix = np.array([[4, 8, -4, 4],
+                       [3, 8, 5, -11],
+                       [-2, 1, 12, -17]])
+
+    values = gaussian_elimination(matrix)
+    print(values)
 
 
 if __name__ == "__main__":
