66ea844f5856c0bf95d9a01c88e94d8f48fcca81
jrpespinas <jrpespinas@gmail.com>
2021-01-07 15:57:28 +0800

    feat: add check root method

--- root_finding/root_finding.py
+++ root_finding/root_finding.py
@@ -1,9 +1,13 @@
 """Root-Finding Algorithms"""
 
-import numpy as np
+# import numpy as np
+
+def check_root(f: 'function', left: float, right: float) -> bool: 
+    return True if f(left) * f(right) < 0 else False
+
 
 def main():
-    pass
+    
 
 if __name__ == "__main__":
     main()
