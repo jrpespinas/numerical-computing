ab8672b771da63ffc53cc828175fd980f167797d
jrpespinas <jrpespinas@gmail.com>
2021-01-07 15:59:49 +0800

    docs: add docstring

--- root_finding/root_finding.py
+++ root_finding/root_finding.py
@@ -3,6 +3,17 @@
 # import numpy as np
 
 def check_root(f: 'function', left: float, right: float) -> bool: 
+    """Identify if the root exists between two endpoints
+    evaluated by function `f`.
+
+    Args: 
+        f (function): The function used for evaluating the endpoints.
+        left (float): The left endpoint.
+        right (float): The right endpoint.
+
+    Returns:
+        bool: Returns `True` if the root exists, returns `False` otherwise.
+    """   
     return True if f(left) * f(right) < 0 else False
 
 
