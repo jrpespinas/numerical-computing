d5ea4a2173e7e0d43d46cd07a14da9b6d0905b9a
jrpespinas <jrpespinas@gmail.com>
2021-01-08 16:07:27 +0800

    docs: add docstring

--- root_finding/root_finding.py
+++ root_finding/root_finding.py
@@ -18,12 +18,29 @@ def check_root(f: 'function', left: float, right: float) -> bool:
     return True if f(left) * f(right) < 0 else False
 
 def bisection_method(f: 'function', left: float, right: float, 
-    error_bound: float = 1e-7) -> float:
+    tolerance: float = 1e-7) -> float:
+    """Find the root guided by the intermediate value theorem and binary 
+    search algorithm.
+
+    Args: 
+        f (function): The function used for evaluating the endpoints.
+        left (float): The left endpoint.
+        right (float): The right endpoint.
+        tolerance (:obj:`float`, optional): The value which determines when
+            the loop should stop.
+    
+    Returns:
+        midpoint (float): The approximate root.
+
+    Examples:
+        >>> approximate_root = bisection_method(math.sin, 2, 4)
+        >>> approximate_root = bisection_method(math.cos, 3, 4, 1e-10)
+    """
     
     assert check_root(f, left, right), \
         "ERROR: Root does not exist between {} and {}.".format(left, right)
 
-    while abs(right - left) > error_bound:
+    while abs(right - left) > tolerance:
         midpoint = (left + right) / 2
         if f(midpoint) * f(left) < 0:
             right = midpoint
