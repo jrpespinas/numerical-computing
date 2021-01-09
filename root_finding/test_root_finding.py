b70431ca4e68bf911bd55d8ecd6b46355be74aad
jrpespinas <jrpespinas@gmail.com>
2021-01-09 12:34:41 +0800

    test: add tests for bisection_method

--- root_finding/test_root_finding.py
+++ root_finding/test_root_finding.py
@@ -9,3 +9,9 @@ class TestMethods:
 
     def test_check_root_exist_2(self):
         assert check_root_exist(math.sin, 3.9, 4) == False
+
+    def test_bisection_method(self):
+        assert bisection_method(math.sin, 2, 4) >= 3.14
+
+    def test_bisection_method_2(self):
+        assert bisection_method(math.sin, 3, 4) >= 3.14
